8e5bbaf46ba6a29c2551831dcbe8fa7e6e0acc3d
SilentHubba <79621066+SilentHubba@users.noreply.github.com>
2025-05-25 19:31:37 -0700

    removed notification file

--- notification.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def send_notification(title, message):
-    script = f'display notification "{message}" with title "{title}"'
-    subprocess.run(["osascript", "-e", script])
-
-# Example usage
-send_notification("Hello!", "This is your macOS notification.")
